6d9703720028568da812c034676d93f6e55b6be3
bashz <nemlaghi.bechir@gmail.com>
2019-10-27 11:56:46 +0100

    fixtures filled as data on each test

--- test/basic.js
+++ test/basic.js
@@ -1,10 +1,122 @@
-var Sails = require('sails').Sails
-var supertest = require('supertest')
+const fs = require('fs')
+const Sails = require('sails').Sails
+const supertest = require('supertest')
+const champs = require('./data/champs')
+const items = require('./data/items')
 
-describe('Basic tests', function () {
+const fillData = async (log) => {
+  log.debug('│   ├── Adding flat Items')
+  for (let i = 0; i < items.length; i++) {
+    let item = items[i]
+    let tags = []
+    for (let j = 0; j < item.tags.length; j++) {
+      if (!j && !i) log.debug('│   │   └── Adding Items Tags')
+      let tag = item.tags[j]
+      try {
+        let tagged = await Tag.findOrCreate({ name: tag }, { name: tag })
+        tags.push(tagged.id)
+      } catch (e) {
+        return e
+      }
+    }
+    try {
+      await Item.create({
+        name: item.name,
+        key: parseInt(item.key),
+        price: item.price,
+        totalPrice: item.timeout,
+        stats: item.stats,
+        tags
+      })
+    } catch (e) {
+      return e
+    }
+  }
+  log.debug('│   ├── linking Items')
+  for (let i = 0; i < items.length; i++) {
+    let item = items[i]
+    try {
+      let from = await Item.find({ key: item.from })
+      await Item.update({ key: item.key }).set({ from: from.map(composite => composite.id) })
+    } catch (e) {
+      return e
+    }
+  }
+  log.debug('│   ├── Adding Champions')
+  for (let i = 0; i < champs.length; i++) {
+    let champ = champs[i]
+    let tags = []
+    let skins = []
+    for (let j = 0; j < champ.tags.length; j++) {
+      if (!j && !i) log.debug('│   │   ├── Adding Champions Tags')
+      let tag = champ.tags[j]
+      try {
+        let tagged = await Tag.findOrCreate({ name: tag }, { name: tag })
+        tags.push(tagged.id)
+      } catch (e) {
+        return e
+      }
+    }
+    for (let j = 0; j < champ.skins.length; j++) {
+      if (!j && !i) log.debug('│   │   ├── Adding Skins')
+      let skin = champ.skins[j]
+      try {
+        let worn = await Skin.create({
+          name: skin.name,
+          chromas: skin.chromas,
+          image: `http://ddragon.leagueoflegends.com/cdn/img/champion/loading/${champ.name}_${skin.num}.jpg`
+        }).fetch()
+        skins.push(worn.id)
+      } catch (e) {
+        return e
+      }
+    }
+    try {
+      let image = fs.readFileSync(`./test/data/pics/${champ.name}.png`, 'binary')
+      if (!i) log.debug('│   │   └── Adding Statistics of Champions')
+      let stats = await Stats.create(champ.stats).fetch()
+      await Champion.create({
+        name: champ.name,
+        title: champ.title,
+        image: image,
+        resource: champ.resource,
+        skins,
+        tags,
+        stats: stats.id
+      })
+    } catch (e) {
+      return e
+    }
+  }
+  log.debug('│   └── Finding recommended items for champions')
+  for (let i = 0; i < champs.length; i++) {
+    let champ = champs[i]
+    try {
+      let champion = await Champion.findOne({ name: champ.name })
+      for (let j = 0; j < champ.recommended.length; j++) {
+        let recommended = champ.recommended[j]
+        let item = await Item.findOne({ key: recommended.key })
+        if (champion && item) {
+          await Recommended.create({
+            map: recommended.map,
+            mode: recommended.mode,
+            type: recommended.type,
+            item: item.id,
+            champion: champion.id
+          })
+        }
+      }
+    } catch (e) {
+      return e
+    }
+  }
+  return null
+}
+
+describe('Basic tests', () => {
   var sails
-  before(function (done) {
-    this.timeout(11000)
+  before(done => {
+    // this.timeout(30000)
     Sails().lift({
       appPath: __dirname + '/instance',
       hooks: {
@@ -15,43 +127,39 @@ describe('Basic tests', function () {
         "views": false,
         "i18n": false
       },
-      models: { migrate: 'alter'},
+      models: { migrate: 'drop' },
       log: { level: 'debug' }
-    }, async function (err, _sails) {
-      // const u = await Organization.create({name: 'org'}).fetch()
-      // const v = await Project.create({name: 'pro', organization: 1}).fetch()
-      // console.log(u, v)
+    }, async (err, _sails) => {
       if (err) {
         return done(err)
       }
+      _sails.log.debug('Mock')
+      _sails.log.debug('├── Start Adding data')
+      err = await fillData(_sails.log)
+      if (err) {
+        _sails.log.error(new Error('Error while creating mocked data'))
+        return done(err)
+      }
+      _sails.log.debug('└── Done adding data')
       sails = _sails
       return done()
     })
   })
 
-  after(function (done) {
+  after(done => {
     if (sails) {
       return sails.lower(done)
     }
     return done()
   })
 
-  it('Hits the configured route', function (done) {
+  it('Hits the configured route', done => {
     supertest(sails.hooks.http.app)
       .post('/graphql')
-      // .send({query: 'mutation {createOrganization (input: {name: "sworm"}) {id name}}'})
-      .send({query: 'mutation {updateOrganization (id: 4, input: {name: "sworme"}) {id name createdAt updatedAt profile{displayName}}}'})
-      // .send({query: 'mutation {deleteOrganization (id: 3) {id}}'})
+      .send({ query: '{champion (name: Nunu) { title items {name} } }' })
       .expect((res) => {
         console.log(res.body)
       })
       .expect(200, done)
-    // supertest(sails.hooks.http.app)
-    //   .post('/graphql')
-    //   .send({query: '{profiles (last: 1) {id displayName age}}'})
-    //   .expect((res) => {
-    //     console.log(res.body)
-    //   })
-    //   .expect(200, done)
   })
 })
